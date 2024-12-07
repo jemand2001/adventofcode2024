5b10dc11e7c2a3c9a53bdfaebd34708787d1d5d2
jemand2001 <jmand@gmx.de>
2024-12-07 12:34:34 +0100

    made Day7.possibleValues generic on possible binary operators

--- app/Day7.hs
+++ app/Day7.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
 module Day7 (day7) where
 
 import Data.Char (isDigit)
@@ -6,24 +7,20 @@ import Data.Char (isDigit)
 day7 :: IO ()
 day7 = interact $ show . part2 . map ((\(v : vs) -> (read (takeWhile isDigit v), map read vs)) . words) . lines
 
-possibleValues :: [Int] -> [Int]
-possibleValues [] = []
-possibleValues [x] = [x]
-possibleValues (x : y : xs) = possibleValues (x + y : xs) ++ possibleValues (x * y : xs)
+type Operator = Int -> Int -> Int
+
+possibleValues :: [Operator] -> [Int] -> [Int]
+possibleValues _ [] = []
+possibleValues _ [x] = [x]
+possibleValues ops (x : y : xs) = concatMap (possibleValues ops . (\op -> op x y : xs)) ops
 
 part1 :: [(Int, [Int])] -> Int
 part1 = sum . map fst . filter (uncurry hasSolution)
   where
-    hasSolution x vs = x `elem` possibleValues vs
-
-possibleValues' :: [Int] -> [Int]
-possibleValues' [] = []
-possibleValues' [x] = [x]
-possibleValues' (x : y : xs) = possibleValues' (x + y : xs) ++ possibleValues' (x * y : xs) ++ possibleValues' (x ||| y : xs)
-  where
-    a ||| b = read $ show a ++ show b
+    hasSolution x vs = x `elem` possibleValues [(+), (*)] vs
 
 part2 :: [(Int, [Int])] -> Int
 part2 = sum . map fst . filter (uncurry hasSolution)
   where
-    hasSolution x vs = x `elem` possibleValues' vs
+    hasSolution x vs = x `elem` possibleValues [(+), (*), (|||)] vs
+    a ||| b = read $ show a ++ show b
